34ecd7aea5167cecab1cd12e3ccc6e968493b9fc
tbistr <tbistr99@gmail.com>
2025-02-27 14:36:27 +0900

    fix: Add uniq consts.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 type Bindings = {
 	DB: D1Database;
 };
 
 export const api = new Hono<{ Bindings: Bindings }>();
+api.use(logger());
 
 const routes = api
 	.get("/clock", (c) => {
@@ -21,17 +23,25 @@ const routes = api
 		const r = await c.env.DB.prepare(
 			"SELECT * FROM messages ORDER BY count ASC;",
 		).all();
-		return c.json(r.results);
+		return c.json(
+			r.results.map((row) => ({ message: row.message, count: row.count })),
+		);
 	})
 	.post("/messages/:message", async (c) => {
 		const message = c.req.param("message");
-		console.log("message", message);
 		await c.env.DB.prepare(
-			"INSERT INTO messages (message, count) VALUES (?, COALESCE((SELECT MAX(count) + 1 FROM messages), 1));",
+			`INSERT INTO messages (message, count)
+			VALUES (?, 1)
+			ON CONFLICT(message) DO UPDATE 
+			SET count = count + 1;`,
 		)
 			.bind(message)
 			.run();
 		return c.json({ message });
+	})
+	.delete("/messages", async (c) => {
+		await c.env.DB.prepare("DELETE FROM messages;").run();
+		return c.json({ message: "All messages deleted" });
 	});
 
 export type AppType = typeof routes;
