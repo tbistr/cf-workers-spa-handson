de8552ad6037286223d89816b8c162696a488011
tbistr <tbistr99@gmail.com>
2025-02-27 14:23:37 +0900

    feat: Add database.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
 
-export const api = new Hono();
+type Bindings = {
+	DB: D1Database;
+};
+
+export const api = new Hono<{ Bindings: Bindings }>();
 
 const routes = api
 	.get("/clock", (c) => {
@@ -12,6 +16,22 @@ const routes = api
 		return c.json({
 			message: "Hello, World!",
 		});
+	})
+	.get("/messages", async (c) => {
+		const r = await c.env.DB.prepare(
+			"SELECT * FROM messages ORDER BY count ASC;",
+		).all();
+		return c.json(r.results);
+	})
+	.post("/messages/:message", async (c) => {
+		const message = c.req.param("message");
+		console.log("message", message);
+		await c.env.DB.prepare(
+			"INSERT INTO messages (message, count) VALUES (?, COALESCE((SELECT MAX(count) + 1 FROM messages), 1));",
+		)
+			.bind(message)
+			.run();
+		return c.json({ message });
 	});
 
 export type AppType = typeof routes;
