ba384470444c42650eaf9eb333ee296425b999dd
tbistr <tbistr99@gmail.com>
2025-03-02 21:32:52 +0900

    feat: Add DB and UI.

--- src/client.tsx
+++ src/client.tsx
@@ -1,97 +1,144 @@
 import { hc } from "hono/client";
+import type { InferRequestType, InferResponseType } from "hono/client";
 import { useState } from "react";
 import { createRoot } from "react-dom/client";
-import type { AppType } from "./api";
+import type { AppType } from "./api/note";
+import type { Note } from "./api/note";
 
 const client = hc<AppType>("/api");
 
 function App() {
 	return (
 		<>
-			<h1>Hello, Hono with React!</h1>
-			<h2>Example of useState</h2>
-			<Counter />
-			<h2>Example of API fetch</h2>
-			<ClockButton />
+			<h1>Note App</h1>
 
-			<h1>Example of working with the database</h1>
-			<h2>Post a message</h2>
-			<PostMessage />
-			<h2>Get all messages</h2>
-			<GetMessages />
+			<h2>Create Note</h2>
+			<CreateNote />
+
+			<h2>Show Notes</h2>
+			<ShowNotes />
+
+			<h2>Show Note</h2>
+			<ShowNote />
+
+			<h2>Delete Note</h2>
+			<DeleteNote />
 		</>
 	);
 }
 
-function Counter() {
-	const [count, setCount] = useState(0);
+const CreateNote = () => {
+	const [title, setTitle] = useState("");
+	const [content, setContent] = useState("");
 	return (
-		<button type="button" onClick={() => setCount(count + 1)}>
-			You clicked me {count} times
-		</button>
-	);
-}
-
-const ClockButton = () => {
-	const [response, setResponse] = useState<string | null>(null);
-
-	const handleClick = async () => {
-		const res = await client.clock.$get();
-		const data = res.ok ? await res.json() : "Error fetching time";
-		setResponse(JSON.stringify(data, null, 2));
-	};
-
-	return (
-		<div>
-			<button type="button" onClick={handleClick}>
-				Get Server Time
-			</button>
-			{response && <pre>{response}</pre>}
-		</div>
-	);
-};
-
-const PostMessage = () => {
-	const [message, setMessage] = useState("");
-	const [response, setResponse] = useState<string | null>(null);
-
-	const handleClick = async () => {
-		const res = await client.messages[":message"].$post({ param: { message } });
-		const data = res.ok ? await res.json() : "Error posting message";
-		setResponse(data);
-	};
-
-	return (
-		<div>
-			<input
-				type="text"
-				value={message}
-				onChange={(e) => setMessage(e.target.value)}
-			/>
-			<button type="button" onClick={handleClick}>
-				Post Message
-			</button>
-			{response && <pre>{JSON.stringify(response, null, 2)}</pre>}
-		</div>
+		<form
+			onSubmit={async (e) => {
+				e.preventDefault();
+				(
+					await client.notes.$post({
+						form: {
+							title: title,
+							content: content,
+						},
+					})
+				).url;
+			}}
+		>
+			<label>
+				Title:
+				<input
+					type="text"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+			</label>
+			<label>
+				Content:
+				<input
+					type="text"
+					value={content}
+					onChange={(e) => setContent(e.target.value)}
+				/>
+			</label>
+			<button type="submit">Create</button>
+		</form>
 	);
 };
 
-const GetMessages = () => {
-	const [response, setResponse] = useState<string | null>(null);
-
-	const handleClick = async () => {
-		const res = await client.messages.$get();
-		const data = res.ok ? await res.json() : "Error fetching messages";
-		setResponse(data);
+const ShowNotes = () => {
+	type ResType = InferResponseType<typeof client.notes.$get>;
+	const [note, setNote] = useState<ResType>([]);
+	const onClick = async () => {
+		const res = await (await client.notes.$get()).json();
+		setNote(res);
 	};
-
 	return (
-		<div>
-			<button type="button" onClick={handleClick}>
-				Get Messages
+		<>
+			<button type="button" onClick={onClick}>
+				Show Note IDs
 			</button>
-			{response && <pre>{JSON.stringify(response, null, 2)}</pre>}
-		</div>
+			<ul>
+				{note.map((n) => (
+					<li key={n.id}>{JSON.stringify(n)}</li>
+				))}
+			</ul>
+		</>
+	);
+};
+
+const ShowNote = () => {
+	const [id, setId] = useState("");
+	const [note, setNote] = useState<Note | undefined>(undefined);
+	return (
+		<form
+			onSubmit={async (e) => {
+				e.preventDefault();
+				const res = await client.notes[":id"].$get({
+					param: { id },
+				});
+				if (res.status === 404) {
+					const data = await res.json();
+					setNote(undefined);
+					return;
+				}
+				setNote(await res.json());
+			}}
+		>
+			<label>
+				ID:
+				<input
+					type="number"
+					value={id}
+					onChange={(e) => setId(e.target.value)}
+				/>
+			</label>
+			<button type="submit">Show</button>
+			{note && <pre>{JSON.stringify(note, null, 2)}</pre>}
+		</form>
+	);
+};
+
+const DeleteNote = () => {
+	const [id, setId] = useState("");
+	return (
+		<form
+			onSubmit={async (e) => {
+				e.preventDefault();
+				await client.notes[":id"].$delete({
+					param: { id },
+				});
+			}}
+		>
+			<label>
+				ID:
+				<input
+					type="number"
+					value={id}
+					onChange={(e) => setId(e.target.value)}
+				/>
+			</label>
+			<button type="submit">Delete</button>
+		</form>
 	);
 };
 
